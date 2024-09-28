c7d3b6d8dc564961bd3bf25291ed58f4494bb786
Luis Selvera <ldselvera@gmail.com>
2024-09-27 22:29:48 -0500

    add book generator using llm with langchain

---
+++ book_generator/src/utils.py
@@ -0,0 +1,34 @@
+from langchain.chains import LLMChain
+from langchain.chat_models import ChatOpenAI
+    
+    
+class BaseStructureChain:
+    
+    PROMPT = ''
+    
+    def __init__(self) -> None:
+    
+        self.llm = ChatOpenAI()
+    
+        self.chain = LLMChain.from_string(
+            llm=self.llm,
+            template=self.PROMPT,
+        )
+    
+        self.chain.verbose = True
+    
+    
+class BaseEventChain:
+    
+    PROMPT = ''
+    
+    def __init__(self) -> None:
+    
+        self.llm = ChatOpenAI(model_name='gpt-3.5-turbo-16k')
+    
+        self.chain = LLMChain.from_string(
+            llm=self.llm,
+            template=self.PROMPT,
+        )
+    
+        self.chain.verbose = True
